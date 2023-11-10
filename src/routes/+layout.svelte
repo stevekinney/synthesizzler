d78eaf01fb1fc31a5ede5f4ebdd63dcaaf3ae48e
Steve Kinney <hello@stevekinney.net>
2023-11-10 12:30:27 -0700

    Add models

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import '../app.css';
 </script>
 
-<div class="container mx-auto my-10">
+<div class="container mx-auto my-10 px-4 md:px-2 lg:px-0">
   <nav class="mb-10 bg-blue-50 py-2 px-4 border-2 border-blue-200 rounded">
     <ul class="flex space-x-4">
       <li>
--- src/routes/keys/+page.svelte
+++ src/routes/keys/+page.svelte
@@ -20,16 +20,10 @@
 <div
   class="grid grid-cols-1 gap-10 xl:grid-rows-[3rem_auto] lg:grid-cols-[1fr_1fr]"
 >
-  <ul class="flex gap-1 lg:col-span-2 items-center">
-    <h2
-      class="px-4 py-2 border-2 border-blue-300 bg-blue-100 rounded-l font-semibold last:rounded-r"
-    >
-      Current Notes
-    </h2>
+  <ul class="flex gap-4 lg:col-span-2 items-center">
+    <h2 class="font-semibold">Current Notes</h2>
     {#each $currentNotes as note}
-      <li
-        class="last:rounded-r px-4 py-2 border-2 border-yellow-300 bg-yellow-100"
-      >
+      <li class="px-4 py-2 border-2 rounded border-yellow-300 bg-yellow-100">
         {note}
       </li>
     {/each}
